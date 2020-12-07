0584339f3e236550a7c64008f295231ec3dbf3d1
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2020-12-07 16:25:07 +0400

    sasasa

---
+++ UserLog/BaseSerializable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLog
+{
+    public  abstract class BaseSerializable
+    { 
+        public  T  Run<T> ( string filePath)  where T :class // т это  класс
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream stream = new FileStream("Log.bin", FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    return formatter.Deserialize(stream) as List<LogServer>;
+                }
+                catch
+                {
+                    return new List<LogServer>();
+                }
+                finally
+                {
+                    stream.Close();
+                }
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+}
